63ad68cfa93f1c2432d645a27cb5a6db62d30945
Jotapcinfo <prowlerurbanhc@hmail.com>
2025-06-07 03:33:51 -0300

    Documentacao

--- src/main/java/spring/boot/task1/task/manager/Application.java
+++ src/main/java/spring/boot/task1/task/manager/Application.java
@@ -3,6 +3,11 @@ package spring.boot.task1.task.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "API Task Manager - Por Jefferson Moreno", version = "1.0", description = "Documentação da API"))
+
 @SpringBootApplication
 public class Application {
 
