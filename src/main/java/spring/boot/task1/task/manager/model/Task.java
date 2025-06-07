cd70b47ce1f56efcbed2349280a4eb150ce495cb
Jotapcinfo <prowlerurbanhc@hmail.com>
2025-06-07 03:20:48 -0300

    Criada a entidade Task com mapeamento JPA e enums

---
+++ src/main/java/spring/boot/task1/task/manager/model/Task.java
@@ -0,0 +1,79 @@
+package spring.boot.task1.task.manager.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import spring.boot.task1.task.manager.enums.Priority;
+import spring.boot.task1.task.manager.enums.Status;
+
+@Entity
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String description;
+
+	@Enumerated(EnumType.STRING)
+	private Priority priority;
+
+	@Enumerated(EnumType.STRING)
+	private Status status;
+
+	private LocalDate dueDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Priority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+}
